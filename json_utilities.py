bd71bae81f7bff7ff08820418ab76e18bdc029ec
Roberto Parra <uedsoldier1990@gmail.com>
2024-11-01 18:18:57 -0600

    Added dict to json string method

--- json_utilities.py
+++ json_utilities.py
@@ -8,3 +8,6 @@ def json_to_dict(json_file, encoding='utf-8'):
 def dict_to_json(dict,json_file, ensure_ascii=False):
     with open(json_file,'w') as file:
         json.dump(dict,file,ensure_ascii=ensure_ascii)
+
+def dict_to_json_string(dict, ensure_ascii=False):
+    return json.dumps(dict,ensure_ascii=ensure_ascii)
